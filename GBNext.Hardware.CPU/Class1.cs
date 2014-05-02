e39c29fc9ee3b7fb566c5b7bdf037120001b8d0c
Keyonir <me@joseangelfernandez.es>
2014-05-02 15:19:51 +0200

    Implemented SET b.r

--- GBNext.Hardware.CPU/Class1.cs
+++ GBNext.Hardware.CPU/Class1.cs
@@ -370,6 +370,14 @@ namespace GBNext.Hardware.CPU
                     case 0x45: BIT_b_r(L); break;
                     case 0x46: BIT_b_rm(HL); break;
                     case 0x47: BIT_b_r(A); break;
+                    case 0xC0: SET_b_r(B); break;
+                    case 0xC1: SET_b_r(C); break;
+                    case 0xC2: SET_b_r(D); break;
+                    case 0xC3: SET_b_r(E); break;
+                    case 0xC4: SET_b_r(H); break;
+                    case 0xC5: SET_b_r(L); break;
+                    case 0xC6: SET_b_rm(HL); break;
+                    case 0xC7: SET_b_r(A); break;
                 }
             }
         }
@@ -903,6 +911,34 @@ namespace GBNext.Hardware.CPU
 
             ConsumeCycle(16);
         }
+
+        private void SET_b_r(int register)
+        {
+            var lo = memoryController.GetPosition(PC++);
+            var hi = memoryController.GetPosition(PC++);
+            int bitToSet = (hi << 8) | lo;
+
+            int bitMask = 1 << bitToSet;
+            registers[register] = (byte) (bitMask | registers[register]);
+
+            ConsumeCycle(8);
+        }
+
+        private void SET_b_rm(UInt16 registerMemory)
+        {
+            UInt16 value = memoryController.GetPosition(registerMemory);
+
+            var lo = memoryController.GetPosition(PC++);
+            var hi = memoryController.GetPosition(PC++);
+            int bitToSet = (hi << 8) | lo;
+
+            int bitMask = 1 << bitToSet;
+            memoryController.Write(registerMemory, (byte)(bitMask | value));
+
+            ConsumeCycle(16);
+        }
+
+
         #endregion
 
 
